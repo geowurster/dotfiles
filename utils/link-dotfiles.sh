940761ac346a1c5ff617d9d7efa91e4c23d43737
Kevin Wurster <wursterk@gmail.com>
2018-02-22 22:06:32 -0500

    Package global git configuration.

--- utils/link-dotfiles.sh
+++ utils/link-dotfiles.sh
@@ -6,3 +6,5 @@ ln -s "$(pwd)/dotfiles/zshrc.sh" "${HOME}/.zshrc"
 
 ln -s "$(pwd)/dotfiles/bash_profile.sh" "${HOME}/.bash_profile"
 ln -s "$(pwd)/dotfiles/bashrc.sh" "${HOME}/.bashrc"
+
+ln -s "$(pwd)/dotfiles/gitconfig" "${HOME}/.gitconfig"
