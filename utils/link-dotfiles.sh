61dca7af9208e4979c0fbdb2dc6629c0aa654657
Kevin Wurster <wursterk@gmail.com>
2018-04-24 23:45:03 -0400

    Starting to go all in on zsh configuration.

--- utils/link-dotfiles.sh
+++ utils/link-dotfiles.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-ln -s "$(pwd)/dotfiles/zshenv.sh" "${HOME}/.zshenv"
-ln -s "$(pwd)/dotfiles/zshrc.sh" "${HOME}/.zshrc"
-
-ln -s "$(pwd)/dotfiles/bash_profile.sh" "${HOME}/.bash_profile"
-ln -s "$(pwd)/dotfiles/bashrc.sh" "${HOME}/.bashrc"
-
-ln -s "$(pwd)/dotfiles/gitconfig" "${HOME}/.gitconfig"
+for F in $(pwd)/dotfiles/*; do
+    ln -s "${F}" "${HOME}"/."$(basename $F)"
+done
