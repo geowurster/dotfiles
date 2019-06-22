4a50c410b0997fa2d2335604903562d8d6f60d79
Kevin Wurster <wursterk@gmail.com>
2019-06-22 12:03:38 -0400

    Fix HOMEBREW_MAKE_JOBS.  Closes #13.

--- prezto-modules/my-homebrew/init.zsh
+++ prezto-modules/my-homebrew/init.zsh
@@ -27,6 +27,10 @@ fi
 
 
 export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_MAKE_JOBS=$(sysctl -n hw.ncpu | awk "{print int($1 * 0.75)}")
 
-unset _P
+# For some reason this does not work:
+#   export HOMEBREW_MAKE_JOBS=$(echo "$NCPU" | awk "{printf int(int($1) * 0.75)}")
+# but this does:
+export HOMEBREW_MAKE_JOBS=$(printf %.0f $(echo "$(sysctl -n hw.ncpu) * 0.75" | bc))
+
+unset _P NCPU
