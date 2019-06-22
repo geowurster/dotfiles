464aa091cb279600decf39d2b9dd70c1d86cee2b
Kevin Wurster <wursterk@gmail.com>
2019-06-22 11:36:59 -0400

    Add GNU grep.

--- prezto-modules/my-homebrew/init.zsh
+++ prezto-modules/my-homebrew/init.zsh
@@ -17,5 +17,16 @@ if [[ -d "/usr/local/sbin" ]]; then
     export PATH="/usr/local/sbin:${PATH}"
 fi
 
+
+# The grep formula installs several grep-adjacent utilities with names like
+# 'ggrep'.  This puts them on the path with normal names.
+_P="/usr/local/opt/grep/libexec/gnubin"
+if [[ -d "$_P" ]]; then
+    export PATH="$_P:$PATH"
+fi
+
+
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_MAKE_JOBS=$(sysctl -n hw.ncpu | awk "{print int($1 * 0.75)}")
+
+unset _P
