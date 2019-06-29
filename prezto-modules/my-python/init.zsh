fcb155d7484d883a2199ff38e8c109e74b58a340
Kevin Wurster <wursterk@gmail.com>
2019-06-29 00:47:39 -0400

    Switch to Python 3 and improve config.
    Closes https://github.com/geowurster/Environment/issues/16

--- prezto-modules/my-python/init.zsh
+++ prezto-modules/my-python/init.zsh
@@ -1,30 +1,51 @@
 # Configure Python environment
 
 
-# Just assume environment is configured properly.
-if [[ -x "$(which python)" ]]; then
-    :
+# If Python frameworks are available grab a list of all available versions.
+FRAMEWORK="/Library/Frameworks/Python.framework"
+if [[ -d "$FRAMEWORK" ]]; then
+    VERSIONS=$(ls -r "$FRAMEWORK/Versions" | grep -vi current)
+else
+    VERSIONS=
+fi
 
-# On a Mac look for a Python Framework, likely from python.org
-elif [[ -d "/Library/Frameworks/Python.framework/Versions" ]]; then
 
-    for VERSION in $(echo "2.7 3.7"); do
-        PY_BIN="/Library/Frameworks/Python.framework/Versions/${VERSION}/bin"
-        if [[ -d "${PY_BIN}" ]]; then
-            export PATH="${PY_BIN}:${PATH}"
+if [[ -d "$FRAMEWORK" ]] && [[ "$VERSIONS" ]]; then
+
+    # Add all Python executables to the $PATH, preferring newer versions.
+    # Also add each user bin directory (pip install --user)
+    for V in $(echo "$VERSIONS" | sort); do
+        PY_BIN="$FRAMEWORK/Versions/$V/bin/"
+        INTERP="$PY_BIN/python$V"
+        PY_USER_BIN=$("$INTERP" -c "import os, site; print(os.path.join(site.getuserbase(), 'bin'))")
+        export PATH="$PY_BIN:$PY_USER_BIN:$PATH"
+    done
+
+    # Latest Python version number
+    LATEST=$(echo "$VERSIONS" | head -1)
+
+    # Ensure generic executables like 'python' and 'pip' are on $PATH.
+    # Python 2 provides these, but Python 3 does not.  Note that
+    # 'python-config' already exists, but 'python-config3.7' does not.
+    for UTIL in idle pip pydoc python; do
+
+        TARGET="$FRAMEWORK/Versions/$LATEST/bin/$UTIL"
+        UTIL_PATH="$TARGET$LATEST"
+
+        if [[ ! -f "$TARGET" ]]; then
+            ln -s "$UTIL_PATH" "$TARGET"
+        fi
+        if [[ ! -x "$TARGET" ]]; then
+            echo "ERROR: Failed to configure $(basename "$TARGET"): $UTIL_PATH" >> /dev/stderr
         fi
     done
-    unset PY_BIN
+
+    unset FRAMEWORK INTERP LATEST PIP PY_BIN PY_USER_BIN PYTHON TARGET UTIL_PATH V VERSIONS
+
+# Found a version of Python.  Just assume it is correct.
+elif [[ -x "$(which python)" ]]; then
+    :
 
 else
     return 1
 fi
-
-
-# For 'pip install --user'
-# Note that Python 2 will appear first in the PATH.
-for INTERP in $(echo "python3 python"); do
-    PY_USER_BIN=$(${INTERP} -c "import os, site; print(os.path.join(site.getuserbase(), 'bin'))")
-    export PATH="${PY_USER_BIN}:${PATH}"
-done
-unset INTERP PY_USER_BIN
