556e666c2219c3140c8d30189f06fa4d0ddb762e
Kevin Wurster <wursterk@gmail.com>
2019-07-23 16:49:48 -0400

    Combine Homebrew path overrides.

--- prezto-modules/my-homebrew/init.zsh
+++ prezto-modules/my-homebrew/init.zsh
@@ -6,31 +6,26 @@ if [[ "${OSTYPE}" != darwin* || ! -x /usr/local/Homebrew/bin/brew ]]; then
 fi
 
 
-# Find Homebrew installed utilities first
-if [[ -d "/usr/local/bin" ]]; then
-    export PATH="/usr/local/bin:${PATH}"
-fi
+# Homebrew $PATH overrides.  Applied in order, so the last overrides the first.
+OVERRIDES=(
+    "/usr/local/bin"
+    "/usr/local/sbin"
+    "/usr/local/opt/grep/libexec/gnubin"
+)
+for P in ${OVERRIDES[@]}; do
+    if [[ -d "$P" ]]; then
+        export PATH="$P:$PATH"
+    fi
+done
 
 
-# Recommended by $(brew doctor)
-if [[ -d "/usr/local/sbin" ]]; then
-    export PATH="/usr/local/sbin:${PATH}"
-fi
-
-
-# The grep formula installs several grep-adjacent utilities with names like
-# 'ggrep'.  This puts them on the path with normal names.
-_P="/usr/local/opt/grep/libexec/gnubin"
-if [[ -d "$_P" ]]; then
-    export PATH="$_P:$PATH"
-fi
-
-
+# Homebrew sends anonymous metrics about usage by default.
 export HOMEBREW_NO_ANALYTICS=1
+
 
 # For some reason this does not work:
 #   export HOMEBREW_MAKE_JOBS=$(echo "$NCPU" | awk "{printf int(int($1) * 0.75)}")
 # but this does:
 export HOMEBREW_MAKE_JOBS=$(printf %.0f $(echo "$(sysctl -n hw.ncpu) * 0.75" | bc))
 
-unset _P NCPU
+unset NCPU OVERRIDES P
